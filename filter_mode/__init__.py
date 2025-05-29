55f910bce4010b663804dc9ec83c9170625b920f
Reyhan Respati <reyhan.respati@monash.edu>
2025-05-29 12:15:06 +1000

    Modify __init__.py

--- filter_mode/__init__.py
+++ filter_mode/__init__.py
@@ -1,8 +1,7 @@
 __name__ = "filter_mode"
-__all__ = ["clim", "anom", "filter", "track"]
+__all__ = ["clim", "anom", "filter"]
 __doc__ = '''This module contains the functions required to decompose tropical motion systems based on phase speed into moisture mode, mixed system, and inertio-gravity wave.'''
 
 from . import clim
 from . import anom
 from . import filter
-from . import track
