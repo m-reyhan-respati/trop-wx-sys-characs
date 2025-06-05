6efaf28ad8c468c2e0760dae5193f0947c69581d
Reyhan Respati <reyhan.respati@monash.edu>
2025-06-05 18:36:09 +1000

    Change the version of conda/analysis3

--- env.sh
+++ env.sh
@@ -5,13 +5,17 @@
 # Load the analysis3 conda environment
 module use /g/data/xp65/public/modules
 module load conda/analysis3-25.02
-source /gdata/gb02/mr4682/tobac_env/bin/activate
+source /home/565/mr4682/tobac_env/bin/activate
 
 # Root directory for this repo
 export ROOT=/home/565/${USER}/trop-wx-sys-characs	# Change this to match where you clone this repo
-export MODULES=${ROOT}/filter_mode
-export MODULES=${ROOT}/track_cell
-export MODULES=${ROOT}/submit_job
 
-# Append to our python path
+# Append modules to our python path
+export MODULES=${ROOT}/filter_mode
+export PYTHONPATH=${MODULES}:${PYTHONPATH}
+
+export MODULES=${ROOT}/track_cell
+export PYTHONPATH=${MODULES}:${PYTHONPATH}
+
+export MODULES=${ROOT}/submit_job
 export PYTHONPATH=${MODULES}:${PYTHONPATH}
