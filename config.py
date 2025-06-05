fae10e1eab197cc866535b7136d8eaf6fff308f4
Reyhan Respati <reyhan.respati@monash.edu>
2025-06-05 18:47:44 +1000

    Add IBTrACS data directory

--- config.py
+++ config.py
@@ -6,3 +6,5 @@ ROOT_DIR = os.environ["ROOT"]
 
 ERA5_RT52_DIR = "/g/data/rt52/era5"
 GPM_IA39_DIR = "/g/data/ia39/aus-ref-clim-data-nci/gpm/data/V07"
+
+IBTrACS_FILE = "/g/data/k10/mr4682/data/IBTrACS/IBTrACS.ALL.v04r00.nc"
