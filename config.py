4cf3346ff37698798cad46723d8ab8f439472225
Reyhan Respati <reyhan.respati@monash.edu>
2025-06-06 16:51:42 +1000

    Add the directory of ISCCP OLR data

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ import os
 username = os.environ["USER"]
 ROOT_DIR = os.environ["ROOT"]
 
+ISCCP_OLR_DIR = "/g/data/k10/mr4682/data/ISCCP/txuptp"
 ERA5_RT52_DIR = "/g/data/rt52/era5"
 GPM_IA39_DIR = "/g/data/ia39/aus-ref-clim-data-nci/gpm/data/V07"
 
