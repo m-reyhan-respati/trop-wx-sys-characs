518a0b7271a17b6cfc3c7b130df9e9d4f4d3d423
Reyhan Respati <reyhan.respati@monash.edu>
2025-06-23 14:17:52 +1000

    Add some more directories

--- config.py
+++ config.py
@@ -8,4 +8,9 @@ ISCCP_OLR_DIR = "/g/data/k10/mr4682/data/ISCCP/txuptp"
 ERA5_RT52_DIR = "/g/data/rt52/era5"
 GPM_IA39_DIR = "/g/data/ia39/aus-ref-clim-data-nci/gpm/data/V07"
 
+SCRATCH_ERA5_DIR = "/scratch/k10/mr4682/data/ERA5"
+SCRATCH_GPM_DIR = "/scratch/k10/mr4682/data/GPM"
+SCRATCH_ISCCP_DIR = "/scratch/k10/mr4682/data/ISCCP"
+SCRATCH_TRACK_DIR = "/scratch/k10/mr4682/data/track_isccp_olr"
+
 IBTrACS_FILE = "/g/data/k10/mr4682/data/IBTrACS/IBTrACS.ALL.v04r00.nc"
