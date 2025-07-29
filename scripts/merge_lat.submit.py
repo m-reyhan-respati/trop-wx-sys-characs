510d4f69294c3884cbb27967bc88fd390b1bd310
Reyhan Respati <reyhan.respati@monash.edu>
2025-07-29 23:21:42 +1000

    Use the script to submit a job to merge the output files of the calculated climatology and anomaly of relative vorticity

--- scripts/merge_lat.submit.py
+++ scripts/merge_lat.submit.py
@@ -7,21 +7,21 @@ import submit_job
 script_dir = f"{ROOT_DIR}/scripts/"
 script_filename = "merge_lat.py"
 
-var = "precipitation"
+var = "vo"
 
 env_vars = {}
-env_vars["FILEI_TAIL"] = ".clim.and.anom.2000-2016"
-env_vars["DIRI"] = f"{SCRATCH_GPM_DIR}/3hr_mean_{var}/clim.and.anom/tmp/"
-env_vars["DIRO"] = f"{SCRATCH_GPM_DIR}/3hr_mean_{var}/clim.and.anom/"
-env_vars["LAT_NAME"] = "lat"
+env_vars["FILEI_TAIL"] = ".clim.and.anom.1984-2016"
+env_vars["DIRI"] = f"{SCRATCH_ERA5_DIR}/{var}/tmp/"
+env_vars["DIRO"] = f"{SCRATCH_ERA5_DIR}/{var}/"
+env_vars["LAT_NAME"] = "latitude"
 
 pbs_dir = f"{ROOT_DIR}/pbs_scripts/"
 ncpus = 1
-mem = 900
+mem = 100
 jobfsmem = 1
-queue = "hugemem"
-project = "gb02"
-walltime = "12:00:00"
+queue = "normal"
+project = "if69"
+walltime = "01:00:00"
 storage = "gdata/xp65+scratch/k10"
 command = f"""cd {ROOT_DIR}
 
@@ -30,7 +30,7 @@ source env.sh
 python3 {script_dir}{script_filename}"""
 
 
-'''
+
 levels = [100, 125, 150, 175, 200, 225, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 775, 800, 825, 850, 875, 900, 925, 950, 975, 1000]
 
 jobs_id = []
@@ -54,11 +54,11 @@ for level in levels:
 
 if len(jobs_id) != 0:
     submit_job.check.check_job_status(jobs_id)
+
+
+
+
 '''
-
-
-
-
 jobs_id = []
 
 env_vars["FILEI"] = var
@@ -79,4 +79,4 @@ submit_job.submit.submit_job(env_vars, pbs_script, jobs_id)
 
 if len(jobs_id) != 0:
     submit_job.check.check_job_status(jobs_id)
-
+'''
