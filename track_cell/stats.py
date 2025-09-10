010b82e0ce505f3775205f1f06ade41ffc60a669
Reyhan Respati <reyhan.respati@monash.edu>
2025-09-10 17:24:38 +1000

    Add calc_extreme_mask_gpm and calc_extreme_probability_gpm functions

--- track_cell/stats.py
+++ track_cell/stats.py
@@ -266,7 +266,123 @@ def compositeTLL_gpm(x, max_activity, season, domain):
     composite = x[np.isin(x["n"].values, max_activity_in_domain["cell"].values), ...].mean(dim="n", skipna=True, keep_attrs=True)
     composite = composite.assign_attrs({"long_name": long_name})
 
-    ttest = scipy.stats.ttest_1samp(x.values, np.zeros((len(x[x.dims[1]]), len(x[x.dims[2]]), len(x[x.dims[3]])), dtype=x.values.dtype), axis=0, nan_policy="omit")
+    ttest = scipy.stats.ttest_1samp(x[np.isin(x["n"].values, max_activity_in_domain["cell"].values), ...].values, np.zeros((len(x[x.dims[1]]), len(x[x.dims[2]]), len(x[x.dims[3]])), dtype=x.values.dtype), axis=0, nan_policy="omit")
+    pvalue = xr.DataArray(ttest.pvalue, dims=composite.dims, coords=composite.coords, attrs={"long_name": "p-value of the Two Sided t Statistics of " + composite.attrs["long_name"]})
+
+    return composite, pvalue
+
+def calc_extreme_mask_gpm(field, track, max_activity, threshold, lat, lon, x_size, y_size, grid_res, lagmax, time_res):
+    seasons = {1: "DJF", 2: "DJF", 3: "MAM", 4: "MAM", 5: "MAM", 6: "JJA", 7: "JJA", 8: "JJA", 9: "SON", 10: "SON", 11: "SON", 12: "DJF"}
+    
+    if "long_name" in field.attrs:
+        long_name = field.attrs["long_name"]
+    else:
+        long_name = ""
+    
+    long_name = "Extreme Event of " + long_name
+
+    n_Features = len(max_activity["timestr"].values)
+
+    x = np.arange(-x_size, x_size + grid_res, grid_res)
+    y = np.arange(-y_size, y_size + grid_res, grid_res)
+    
+    instances = np.zeros((n_Features, 2 * lagmax + 1, len(y), len(x))) * np.nan
+
+    lat3 = np.zeros(len(lat.values) + int(2*y_size/grid_res))
+    lat3[0:int(y_size/grid_res)] = np.min(lat.values) + y[0:int(y_size/grid_res)]
+    lat3[int(y_size/grid_res):-int(y_size/grid_res)] = lat.values
+    lat3[-int(y_size/grid_res):] = np.max(lat.values) + y[-int(y_size/grid_res):]
+    
+    lon3 = np.zeros(len(lon.values) * 3)
+    lon3[0*len(lon.values):1*len(lon.values)] = lon.values - 360.0
+    lon3[1*len(lon.values):2*len(lon.values)] = lon.values
+    lon3[2*len(lon.values):3*len(lon.values)] = lon.values + 360.0
+    
+    for i in range(0, n_Features):
+        track_i = track.loc[track["cell"] == max_activity["cell"].values[i]]
+        
+        max_activity_i = max_activity.loc[max_activity["cell"] == max_activity["cell"].values[i]]
+        max_activity_index = np.where(track_i["timestr"].values == max_activity_i["timestr"].values)[0][0]
+        
+        time_i = track_i["timestr"].values
+        lat_i = track_i["latitude"].values
+        lon_i = track_i["longitude"].values
+
+        for lag in range(-lagmax, lagmax + 1):
+            if (max_activity_index + lag >= 0) & (max_activity_index + lag < len(time_i)):
+                time_i_lag = time_i[max_activity_index + lag]
+                lat_i_lag = lat_i[max_activity_index + lag]
+                lon_i_lag = lon_i[max_activity_index + lag]
+                
+                field_i_lag = np.zeros((len(lat3), len(lon3))) * np.nan
+                
+                field_i_lag[int(y_size/grid_res):-int(y_size/grid_res), 0*len(lon.values):1*len(lon.values)] = field.sel(time=time_i_lag).values
+                field_i_lag[int(y_size/grid_res):-int(y_size/grid_res), 1*len(lon.values):2*len(lon.values)] = field.sel(time=time_i_lag).values
+                field_i_lag[int(y_size/grid_res):-int(y_size/grid_res), 2*len(lon.values):3*len(lon.values)] = field.sel(time=time_i_lag).values
+                
+                lat_i_lag_index = np.argmin(np.abs(lat3 - lat_i_lag))
+                lon_i_lag_index = np.argmin(np.abs(lon3 - lon_i_lag))
+                
+                instances_i_lag = field_i_lag[lat_i_lag_index-int(y_size/grid_res):lat_i_lag_index+int(y_size/grid_res)+1, lon_i_lag_index-int(x_size/grid_res):lon_i_lag_index+int(x_size/grid_res)+1]
+                
+                season_i_lag = seasons[pd.to_datetime(time_i_lag).month]
+                
+                threshold3 = np.zeros((len(lat3), len(lon3))) * np.nan
+                threshold3[int(y_size/grid_res):-int(y_size/grid_res), 0*len(lon.values):1*len(lon.values)] = threshold.sel(season=season_i_lag).values
+                threshold3[int(y_size/grid_res):-int(y_size/grid_res), 1*len(lon.values):2*len(lon.values)] = threshold.sel(season=season_i_lag).values
+                threshold3[int(y_size/grid_res):-int(y_size/grid_res), 2*len(lon.values):3*len(lon.values)] = threshold.sel(season=season_i_lag).values
+                
+                threshold_i_lag = threshold3[lat_i_lag_index-int(y_size/grid_res):lat_i_lag_index+int(y_size/grid_res)+1, lon_i_lag_index-int(x_size/grid_res):lon_i_lag_index+int(x_size/grid_res)+1]
+                
+                instances_i_lag = np.where(instances_i_lag > 0.0, instances_i_lag, np.nan)
+                instances_i_lag = np.where((~np.isnan(instances_i_lag)) & (instances_i_lag < threshold_i_lag), 0.0, instances_i_lag)
+                instances_i_lag = np.where(instances_i_lag >= threshold_i_lag, 1.0, instances_i_lag)
+                
+                instances[i, lag + lagmax, :, :] = instances_i_lag
+
+    n = max_activity["cell"].values
+    l = np.arange(-lagmax, lagmax + 1, 1) * time_res
+
+    x = xr.DataArray(x, dims=["lon"], coords={"lon": x}, attrs={"long_name": "Relative Longitude", "units": "degrees_east"})
+    y = xr.DataArray(y, dims=["lat"], coords={"lat": y}, attrs={"long_name": "Relative Latitude", "units": "degrees_north"})
+    l = xr.DataArray(l, dims=["lag"], coords={"lag": l}, attrs={"long_name": "Lag", "units": "hour"})
+    n = xr.DataArray(n, dims=["n"], coords={"n": n}, attrs={"long_name": "Instance #"})
+
+    instances = xr.DataArray(instances, dims=["n", "lag", "lat", "lon"], coords={"n": n, "lag": l, "lat": y, "lon": x}, attrs={"long_name": long_name})
+
+    return instances
+
+def calc_extreme_probability_gpm(x, max_activity, season, domain, H_0):
+    if "long_name" in x.attrs:
+        long_name = x.attrs["long_name"]
+    else:
+        long_name = ""
+
+    if season == "ALL":
+        max_activity_in_season = max_activity
+    elif season == "DJF":
+        max_activity_in_season = max_activity.loc[(pd.to_datetime(max_activity["timestr"]).dt.month == 12) | (pd.to_datetime(max_activity["timestr"]).dt.month <= 2)]
+    elif season == "MAM":
+        max_activity_in_season = max_activity.loc[(pd.to_datetime(max_activity["timestr"]).dt.month >= 3) & (pd.to_datetime(max_activity["timestr"]).dt.month <= 5)]
+    elif season == "JJA":
+        max_activity_in_season = max_activity.loc[(pd.to_datetime(max_activity["timestr"]).dt.month >= 6) & (pd.to_datetime(max_activity["timestr"]).dt.month <= 8)]
+    elif season == "SON":
+        max_activity_in_season = max_activity.loc[(pd.to_datetime(max_activity["timestr"]).dt.month >= 9) & (pd.to_datetime(max_activity["timestr"]).dt.month <= 11)]
+
+    if domain == "EQ":
+        max_activity_in_domain = max_activity_in_season.loc[(max_activity_in_season["latitude"] > -10.0) & (max_activity_in_season["latitude"] < 10.0)]
+        long_name = "Probability of " + long_name + " (" + season + ", EQ)"
+    elif domain == "NH":
+        max_activity_in_domain = max_activity_in_season.loc[(max_activity_in_season["latitude"] >= 10.0) & (max_activity_in_season["latitude"] <= 20.0)]
+        long_name = "Probability of " + long_name + " (" + season + ", NH)"
+    elif domain == "SH":
+        max_activity_in_domain = max_activity_in_season.loc[(max_activity_in_season["latitude"] >= -20.0) & (max_activity_in_season["latitude"] <= -10.0)]
+        long_name = "Probability of " + long_name + " (" + season + ", SH)"
+
+    composite = x[np.isin(x["n"].values, max_activity_in_domain["cell"].values), ...].mean(dim="n", skipna=True, keep_attrs=True)
+    composite = composite.assign_attrs({"long_name": long_name})
+
+    ttest = scipy.stats.ttest_1samp(x[np.isin(x["n"].values, max_activity_in_domain["cell"].values), ...].values, np.ones((len(x[x.dims[1]]), len(x[x.dims[2]]), len(x[x.dims[3]])), dtype=x.values.dtype) * H_0, axis=0, nan_policy="omit")
     pvalue = xr.DataArray(ttest.pvalue, dims=composite.dims, coords=composite.coords, attrs={"long_name": "p-value of the Two Sided t Statistics of " + composite.attrs["long_name"]})
 
     return composite, pvalue
