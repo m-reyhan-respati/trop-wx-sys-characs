dd327a019d003638a38f30bc9bbda784d9435cf1
Reyhan Respati <reyhan.respati@monash.edu>
2025-10-03 15:25:56 +1000

    Add a python script to calculate composites of the total field (not anomaly) of the GPM-IMERG rainfall variables and another python script to submit it to queue

---
+++ scripts/calc_composite_total_field_gpm_track_isccp_olr.submit.py
@@ -0,0 +1,61 @@
+import numpy as np
+from glob import glob
+
+from config import *
+
+import submit_job
+
+script_dir = f"{ROOT_DIR}/scripts/"
+script_filename = "calc_composite_total_field_gpm_track_isccp_olr.py"
+
+lagmax = 0
+
+lags = np.arange(-lagmax, lagmax + 1.0, 1.0) * 3.0
+
+env_vars = {}
+env_vars["FILE_NAME"] = "precipitation"
+env_vars["VAR_NAME"] = "precipitation"
+env_vars["FILTERED"] = 0
+env_vars["DIRI"] = f"{SCRATCH_GPM_DIR}/3hr_mean_{env_vars['FILE_NAME']}/"
+env_vars["DIRO"] = f"{env_vars['DIRI']}composites/"
+
+pbs_dir = f"{ROOT_DIR}/pbs_scripts/"
+ncpus = 1
+mem = 150
+jobfsmem = 1
+queue = "normal"
+project = "fy29"
+walltime = "02:30:00"
+storage = "gdata/xp65+scratch/k10"
+command = f"""cd {ROOT_DIR}
+
+source env.sh
+
+python3 {script_dir}{script_filename}"""
+
+mode_dict = {"Moisture Mode": "moisture.mode", "Mixed System": "mixed.system", "IG Wave": "ig.wave", "Eastward Moisture Mode": "eastward.moisture.mode", "Eastward Mixed System": "eastward.mixed.system", "Eastward IG Wave": "eastward.ig.wave", "Westward Moisture Mode": "westward.moisture.mode", "Westward Mixed System": "westward.mixed.system", "Westward IG Wave": "westward.ig.wave", "Tropical Cyclone": "tc"}
+
+modes = ["Moisture Mode", "Mixed System", "IG Wave", "Eastward Moisture Mode", "Eastward Mixed System", "Eastward IG Wave", "Westward Moisture Mode", "Westward Mixed System", "Westward IG Wave", "Tropical Cyclone"]
+
+for mode in modes[9:10]:
+    jobs_id = []
+    
+    env_vars["MODE"] = mode
+    
+    for lag in lags:
+        env_vars["LAG"] = lag
+        
+        files = sorted(glob(f"{env_vars['DIRO']}composite.{env_vars['FILE_NAME']}.{mode_dict[mode]}.lag.{env_vars['LAG']:.0f}.nc"))
+        name = f"{script_filename[:-3]}__{env_vars['FILE_NAME']}_{mode_dict[mode]}_lag_{env_vars['LAG']:.0f}"
+        
+        if len(files) != 0:
+            continue
+        
+        pbs_filename = f"{name}.sh"
+        
+        pbs_script = submit_job.submit.create_pbs_script(pbs_dir, pbs_filename, ncpus, mem, jobfsmem, queue, project, walltime, storage, name, command)
+        
+        submit_job.submit.submit_job(env_vars, pbs_script, jobs_id)
+    
+    if len(jobs_id) != 0:
+        submit_job.check.check_job_status(jobs_id)
