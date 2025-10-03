97e3ffb2686e60e7d3628e62c13259ab73723e14
Reyhan Respati <reyhan.respati@monash.edu>
2025-10-03 15:37:54 +1000

    Add a python script to calculate the percentage of events of rainfall metrics exceeding some threshold associated with each type of weather systems using the circular mask method and another python script to submit it to queue

---
+++ scripts/calc_mean_torrential_contribution_gpm_track_isccp_olr_circ_mask.py
@@ -0,0 +1,61 @@
+import numpy as np
+import pandas as pd
+import xarray as xr
+from glob import glob
+
+import os
+import sys
+
+from config import *
+from util import *
+
+diri = os.environ.get("DIRI")
+file_name = os.environ.get("FILE_NAME")
+var_name = os.environ.get("VAR_NAME")
+threshold = float(os.environ.get("THRESHOLD"))
+radius = float(os.environ.get("RADIUS"))
+mode = os.environ.get("MODE")
+lat_min = float(os.environ.get("LAT_MIN"))
+lat_max = float(os.environ.get("LAT_MAX"))
+diro = os.environ.get("DIRO")
+
+modes = {"Moisture Mode": "moisture_mode", "Mixed System": "mixed_system", "IG Wave": "ig_wave", "Eastward Moisture Mode": "eastward_moisture_mode", "Eastward Mixed System": "eastward_mixed_system", "Eastward IG Wave": "eastward_ig_wave", "Westward Moisture Mode": "westward_moisture_mode", "Westward Mixed System": "westward_mixed_system", "Westward IG Wave": "westward_ig_wave", "Tropical Cyclone": "tc"}
+
+mode_dict = {"Moisture Mode": "moisture.mode", "Mixed System": "mixed.system", "IG Wave": "ig.wave", "Eastward Moisture Mode": "eastward.moisture.mode", "Eastward Mixed System": "eastward.mixed.system", "Eastward IG Wave": "eastward.ig.wave", "Westward Moisture Mode": "westward.moisture.mode", "Westward Mixed System": "westward.mixed.system", "Westward IG Wave": "westward.ig.wave", "Tropical Cyclone": "tc"}
+
+if lat_min < 0:
+    lat_min_string = f"{np.abs(lat_min + 0.05):.2f}S"
+else:
+    lat_min_string = f"{lat_min + 0.05:.2f}N"
+
+if lat_max < 0:
+    lat_max_string = f"{np.abs(lat_max - 0.05):.2f}S"
+else:
+    lat_max_string = f"{lat_max - 0.05:.2f}N"
+
+fileo = f"{diro}mean.torrential.contribution.{file_name}.w.t.{threshold:.2f}.{mode_dict[mode]}.circ.mask.{radius:.1f}km.{lat_min_string}-{lat_max_string}.nc"
+
+def _preprocess(ds):
+    return ds.sel(lat=slice(lat_min + 0.01, lat_max - 0.01))
+
+files = sorted(glob(f"{diri}torrential.contribution.{file_name}.w.t.{threshold:.2f}.*.circ.mask.{radius:.1f}km.nc"))
+
+ds = xr.open_mfdataset(files, preprocess=_preprocess)
+
+x = ds[f"torrential_contribution_{var_name}_{modes[mode]}"]
+
+x_all = x.mean(dim="time", skipna=True).compute()
+
+dso = xr.Dataset()
+dso[f"mean_torrential_contribution_{var_name}_{modes[mode]}_ALL"] = x_all
+
+seasons = ["DJF", "MAM", "JJA", "SON"]
+
+for season in seasons:
+    x_season = x[x["time"].dt.season.values == season, :, :].mean(dim="time", skipna=True).compute()
+    
+    dso[f"mean_torrential_contribution_{var_name}_{modes[mode]}_{season}"] = x_season
+
+print(dso)
+
+dso.to_netcdf(path=fileo, format="NETCDF4")
---
+++ scripts/calc_mean_torrential_contribution_gpm_track_isccp_olr_circ_mask.submit.py
@@ -0,0 +1,72 @@
+import numpy as np
+from glob import glob
+
+from config import *
+
+import submit_job
+
+script_dir = f"{ROOT_DIR}/scripts/"
+script_filename = "calc_mean_torrential_contribution_gpm_track_isccp_olr_circ_mask.py"
+
+lat_min_list = np.arange(-30.5, 30.5, 2.0)
+lat_max_list = lat_min_list + 2.0
+lat_max_list[-1] = 30.5
+
+env_vars = {}
+env_vars["FILE_NAME"] = "precipitation"
+env_vars["VAR_NAME"] = "precipitation"
+env_vars["THRESHOLD"] = 0.0
+env_vars["RADIUS"] = 1000.0
+env_vars["DIRI"] = f"{SCRATCH_GPM_DIR}/3hr_mean_{env_vars['FILE_NAME']}/torrential_contribution/circ_mask/"
+env_vars["DIRO"] = f"{SCRATCH_GPM_DIR}/3hr_mean_{env_vars['FILE_NAME']}/mean_torrential_contribution/circ_mask/"
+
+pbs_dir = f"{ROOT_DIR}/pbs_scripts/"
+ncpus = 1
+mem = 190
+jobfsmem = 1
+queue = "normal"
+project = "k10"
+walltime = "02:30:00"
+storage = "gdata/xp65+scratch/k10"
+command = f"""cd {ROOT_DIR}
+
+source env.sh
+
+python3 {script_dir}{script_filename}"""
+
+mode_dict = {"Moisture Mode": "moisture.mode", "Mixed System": "mixed.system", "IG Wave": "ig.wave", "Eastward Moisture Mode": "eastward.moisture.mode", "Eastward Mixed System": "eastward.mixed.system", "Eastward IG Wave": "eastward.ig.wave", "Westward Moisture Mode": "westward.moisture.mode", "Westward Mixed System": "westward.mixed.system", "Westward IG Wave": "westward.ig.wave", "Tropical Cyclone": "tc"}
+
+modes = ["Moisture Mode", "Mixed System", "IG Wave", "Eastward Moisture Mode", "Eastward Mixed System", "Eastward IG Wave", "Westward Moisture Mode", "Westward Mixed System", "Westward IG Wave", "Tropical Cyclone"]
+
+for mode in modes[0:3]:
+    jobs_id = []
+    
+    env_vars["MODE"] = mode
+    
+    for i in range(0, len(lat_min_list)):
+        env_vars["LAT_MIN"] = lat_min_list[i]
+        env_vars["LAT_MAX"] = lat_max_list[i]
+        
+        if lat_min_list[i] < 0:
+            lat_min_string = f"{np.abs(lat_min_list[i] + 0.05):.2f}S"
+        else:
+            lat_min_string = f"{lat_min_list[i] + 0.05:.2f}N"
+        if lat_max_list[i] < 0:
+            lat_max_string = f"{np.abs(lat_max_list[i] - 0.05):.2f}S"
+        else:
+            lat_max_string = f"{lat_max_list[i] - 0.05:.2f}N"
+        
+        files = sorted(glob(f"{env_vars['DIRO']}mean.torrential.contribution.{env_vars['FILE_NAME']}.w.t.{env_vars['THRESHOLD']:.2f}.{mode_dict[mode]}.circ.mask.{env_vars['RADIUS']:.1f}km.{lat_min_string}-{lat_max_string}.nc"))
+        name = f"{script_filename[:-3]}__{env_vars['FILE_NAME']}_{env_vars['THRESHOLD']:.2f}_{mode_dict[mode]}_circ_mask_{env_vars['RADIUS']:.1f}km_{lat_min_string}-{lat_max_string}"
+        
+        if len(files) != 0:
+            continue
+        
+        pbs_filename = f"{name}.sh"
+        
+        pbs_script = submit_job.submit.create_pbs_script(pbs_dir, pbs_filename, ncpus, mem, jobfsmem, queue, project, walltime, storage, name, command)
+        
+        submit_job.submit.submit_job(env_vars, pbs_script, jobs_id)
+    
+    if len(jobs_id) != 0:
+        submit_job.check.check_job_status(jobs_id)
